d39eca767caee0183f48c9697b20e5c0713072cb
A19057 <12345678>
2020-08-27 18:41:16 +0800

    修改orderby的bug

--- src/test/java/com/antgroup/exam/OrderByTest.java
+++ src/test/java/com/antgroup/exam/OrderByTest.java
@@ -22,9 +22,9 @@ public class OrderByTest {
         List<Student> queryResult = new DataQuery<Student, String>()
                 .query(students, null, orderBy, null, null);
         int preGender = queryResult.get(0).getGender();
-        for (int i = 0; i < queryResult.size(); i++) {
-            Assert.assertTrue(queryResult.get(i).getGender() >= preGender);
-            preGender = queryResult.get(i).getGender();
+        for (Student student : queryResult) {
+            Assert.assertTrue(student.getGender() >= preGender);
+            preGender = student.getGender();
         }
     }
     //                .add("age", OrderDirection.ASC);
